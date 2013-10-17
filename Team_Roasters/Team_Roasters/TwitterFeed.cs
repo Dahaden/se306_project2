0f2635e45e2f482e0dec4631d81112dd7f07c47c
johnlmbrt <jlam730@aucklanduni.ac.nz>
2013-10-17 10:11:57 +0100

    added twitter stuff to project

---
+++ Team_Roasters/Team_Roasters/TwitterFeed.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace Team_Roasters
+{
+    class TwitterFeed
+    {
+        List<Tweet> tweets;
+
+        private string getTwitterFeed()
+        {
+            var oauth_token = "1963695506-KINm9HlNreOn3Oi0xYU5FqEuZ5CnyrdtVX35Oi6";
+            var oauth_token_secret = "l246EjoE4qyr0EcRZQpQM8i4QttjTp3PK6CEcoX1Vhs";
+            var oauth_consumer_key = "1AoVDPg12ID2HRPnVIOWDQ";
+            var oauth_consumer_secret = "LNycb2olr2k3d2tGSpmkWWUMhQfJtDfwoJ2HJWx6cU";
+
+            // oauth implementation details
+            var oauth_version = "1.0";
+            var oauth_signature_method = "HMAC-SHA1";
+
+            // unique request details
+            var oauth_nonce = Convert.ToBase64String(
+                new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            var timeSpan = DateTime.UtcNow
+                - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var oauth_timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+
+            // message api details
+            //var status = "Updating status via REST API if this works";
+            var resource_url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+            var screen_name = "ChildCancerNZ";
+            // create oauth signature
+            var baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+                            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&screen_name={6}";
+
+            var baseString = string.Format(baseFormat,
+                                        oauth_consumer_key,
+                                        oauth_nonce,
+                                        oauth_signature_method,
+                                        oauth_timestamp,
+                                        oauth_token,
+                                        oauth_version,
+                                         Uri.EscapeDataString(screen_name)
+                                        );
+
+            baseString = string.Concat("GET&", Uri.EscapeDataString(resource_url), "&", Uri.EscapeDataString(baseString));
+
+            var compositeKey = string.Concat(Uri.EscapeDataString(oauth_consumer_secret),
+                                    "&", Uri.EscapeDataString(oauth_token_secret));
+
+            string oauth_signature;
+            using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+            {
+                oauth_signature = Convert.ToBase64String(
+                    hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
+            }
+
+            // create the request header
+            var headerFormat = "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+                               "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+                               "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+                               "oauth_version=\"{6}\"";
+
+            var authHeader = string.Format(headerFormat,
+                                    Uri.EscapeDataString(oauth_nonce),
+                                    Uri.EscapeDataString(oauth_signature_method),
+                                    Uri.EscapeDataString(oauth_timestamp),
+                                    Uri.EscapeDataString(oauth_consumer_key),
+                                    Uri.EscapeDataString(oauth_token),
+                                    Uri.EscapeDataString(oauth_signature),
+                                    Uri.EscapeDataString(oauth_version)
+                            );
+
+
+            // make the request
+
+            ServicePointManager.Expect100Continue = false;
+
+            var postBody = "screen_name=" + Uri.EscapeDataString(screen_name);//
+            resource_url += "?" + postBody;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resource_url);
+            request.Headers.Add("Authorization", authHeader);
+            request.Method = "GET";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            WebResponse response = request.GetResponse();
+            string responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseData;
+        }
+
+        public void updateTweets()
+        {
+            tweets = JsonConvert.DeserializeObject<List<Tweet>>(getTwitterFeed());
+
+            foreach (Tweet t in tweets)
+            {
+                //plug in values for each tweet
+                //Tweet author name
+                //Tweet author @username
+                //Tweet author avatar
+                //Tweet text
+                //Tweet timestamp
+                //Tweet entity
+            }
+        }
+
+    }
+
+    public class Tweet
+    {
+        public string created_at { get; set; }
+        public long id { get; set; }
+        public string id_str { get; set; }
+        public string text { get; set; }
+        public string source { get; set; }
+        public bool truncated { get; set; }
+        public object in_reply_to_status_id { get; set; }
+        public object in_reply_to_status_id_str { get; set; }
+        public object in_reply_to_user_id { get; set; }
+        public object in_reply_to_user_id_str { get; set; }
+        public object in_reply_to_screen_name { get; set; }
+        public User user { get; set; }
+        public object geo { get; set; }
+        public object coordinates { get; set; }
+        public object place { get; set; }
+        public object contributors { get; set; }
+        public int retweet_count { get; set; }
+        public int favorite_count { get; set; }
+        public Entities2 entities { get; set; }
+        public bool favorited { get; set; }
+        public bool retweeted { get; set; }
+        public bool possibly_sensitive { get; set; }
+        public string lang { get; set; }
+    }
+
+    public class Url2
+    {
+        public string url { get; set; }
+        public string expanded_url { get; set; }
+        public string display_url { get; set; }
+        public List<int> indices { get; set; }
+    }
+
+    public class Url
+    {
+        public List<Url2> urls { get; set; }
+    }
+
+    public class Description
+    {
+        public List<object> urls { get; set; }
+    }
+
+    public class Entities
+    {
+        public Url url { get; set; }
+        public Description description { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string id_str { get; set; }
+        public string name { get; set; }
+        public string screen_name { get; set; }
+        public string location { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public Entities entities { get; set; }
+        public bool @protected { get; set; }
+        public int followers_count { get; set; }
+        public int friends_count { get; set; }
+        public int listed_count { get; set; }
+        public string created_at { get; set; }
+        public int favourites_count { get; set; }
+        public int utc_offset { get; set; }
+        public string time_zone { get; set; }
+        public bool geo_enabled { get; set; }
+        public bool verified { get; set; }
+        public int statuses_count { get; set; }
+        public string lang { get; set; }
+        public bool contributors_enabled { get; set; }
+        public bool is_translator { get; set; }
+        public string profile_background_color { get; set; }
+        public string profile_background_image_url { get; set; }
+        public string profile_background_image_url_https { get; set; }
+        public bool profile_background_tile { get; set; }
+        public string profile_image_url { get; set; }
+        public string profile_image_url_https { get; set; }
+        public string profile_banner_url { get; set; }
+        public string profile_link_color { get; set; }
+        public string profile_sidebar_border_color { get; set; }
+        public string profile_sidebar_fill_color { get; set; }
+        public string profile_text_color { get; set; }
+        public bool profile_use_background_image { get; set; }
+        public bool default_profile { get; set; }
+        public bool default_profile_image { get; set; }
+        public object following { get; set; }
+        public bool follow_request_sent { get; set; }
+        public object notifications { get; set; }
+    }
+
+    public class Url3
+    {
+        public string url { get; set; }
+        public string expanded_url { get; set; }
+        public string display_url { get; set; }
+        public List<int> indices { get; set; }
+    }
+
+    public class UserMention
+    {
+        public string screen_name { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+        public string id_str { get; set; }
+        public List<int> indices { get; set; }
+    }
+
+    public class Small
+    {
+        public int w { get; set; }
+        public int h { get; set; }
+        public string resize { get; set; }
+    }
+
+    public class Thumb
+    {
+        public int w { get; set; }
+        public int h { get; set; }
+        public string resize { get; set; }
+    }
+
+    public class Large
+    {
+        public int w { get; set; }
+        public int h { get; set; }
+        public string resize { get; set; }
+    }
+
+    public class Medium2
+    {
+        public int w { get; set; }
+        public int h { get; set; }
+        public string resize { get; set; }
+    }
+
+    public class Sizes
+    {
+        public Small small { get; set; }
+        public Thumb thumb { get; set; }
+        public Large large { get; set; }
+        public Medium2 medium { get; set; }
+    }
+
+    public class Medium
+    {
+        public long id { get; set; }
+        public string id_str { get; set; }
+        public List<int> indices { get; set; }
+        public string media_url { get; set; }
+        public string media_url_https { get; set; }
+        public string url { get; set; }
+        public string display_url { get; set; }
+        public string expanded_url { get; set; }
+        public string type { get; set; }
+        public Sizes sizes { get; set; }
+    }
+
+    public class Entities2
+    {
+        public List<object> hashtags { get; set; }
+        public List<object> symbols { get; set; }
+        public List<Url3> urls { get; set; }
+        public List<UserMention> user_mentions { get; set; }
+        public List<Medium> media { get; set; }
+    }
+}
