a68df5b43032072b989b8a71ac5eac52674321cc
Georgina Wood <georgina.wood@clear.net.nz>
2013-10-21 10:59:51 +1300

    In the process of changing build in text to loaded documents.

--- Team_Roasters/Team_Roasters/Screens/WhatWeDo.xaml.cs
+++ Team_Roasters/Team_Roasters/Screens/WhatWeDo.xaml.cs
@@ -1,13 +1,29 @@
-﻿namespace Team_Roasters.Screens
+﻿using Team_Roasters;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.IO;
+using System;
+using HtmlAgilityPack;
+using System.Text;
+using System.Xml;
+using System.Net;
+namespace Team_Roasters.Screens
 {
     /// <summary>
     /// Interaction logic for FamilySupportScreen.xaml
     /// </summary>
     public partial class WhatWeDo : Screen
     {
+        // Creates a FlowDocument from the finished saved .xaml file
+        FlowDocument flowDocument = (FlowDocument)XamlReader.Load(File.OpenRead("../../Resources/Ambassadors/Amb_AB.xaml"));
+            
         public WhatWeDo(SurfaceWindow1 parentWindow) : base(parentWindow)
         {
-            InitializeComponent();           
+            InitializeComponent();
+            
+            // Passes the document into the RichTextBox which displays the formatted contents in the app
+            AB_text.Document = flowDocument;
         }
 
         private void SurfaceButton_Back(object sender, System.Windows.RoutedEventArgs e)
@@ -22,6 +38,9 @@
             Our_People.Visibility = System.Windows.Visibility.Collapsed;
             Our_History.Visibility = System.Windows.Visibility.Collapsed;
             How_We_Help.Visibility = System.Windows.Visibility.Collapsed;
+
+            // Passes the document into the RichTextBox which displays the formatted contents in the app
+            AB_text.Document = flowDocument;
         }
 
         private void SurfaceButton_OurPeople(object sender, System.Windows.RoutedEventArgs e)
