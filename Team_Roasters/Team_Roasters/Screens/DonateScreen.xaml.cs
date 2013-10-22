27301e7afc08b7034d2fe53a649c2731c33efbfa
johnlmbrt <jlam730@aucklanduni.ac.nz>
2013-10-22 16:44:34 +0100

    added what your money buys to donate page

--- Team_Roasters/Team_Roasters/Screens/DonateScreen.xaml.cs
+++ Team_Roasters/Team_Roasters/Screens/DonateScreen.xaml.cs
@@ -1,3 +1,11 @@
+using System.Windows.Media;
+using System.Windows;
+using Team_Roasters;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.IO;
+
 namespace Team_Roasters.Screens
 {
     /// <summary>
@@ -7,7 +15,10 @@ namespace Team_Roasters.Screens
     {
         public DonateScreen(SurfaceWindow1 parentWindow) : base(parentWindow)
         {
-            InitializeComponent();           
+            InitializeComponent();
+            setButtonColours();
+            FlowDocument flowDocument = (FlowDocument)XamlReader.Load(File.OpenRead("../../Resources/Donate/WhatYourMoneyBuys.xaml"));
+            moneyBuysViewer.Document = flowDocument;
         }
 
         private void SurfaceButton_Back(object sender, System.Windows.RoutedEventArgs e)
@@ -15,6 +26,28 @@ namespace Team_Roasters.Screens
             parentWindow.popScreen();
         }
 
+        private void DonateNow_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DonateNow.Visibility = System.Windows.Visibility.Visible;
+            WhatYourMoneyBuys.Visibility = System.Windows.Visibility.Collapsed;
+            DonateNow_Button.Style = (Style)FindResource("SelectedButton");
+            WhatYourMoneyBuys_Button.Style = (Style)FindResource("NotSelectedButton");
+        }
+
+        private void WhatYourMoneyBuys_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DonateNow.Visibility = System.Windows.Visibility.Collapsed;
+            WhatYourMoneyBuys.Visibility = System.Windows.Visibility.Visible;
+            DonateNow_Button.Style = (Style)FindResource("NotSelectedButton");
+            WhatYourMoneyBuys_Button.Style = (Style)FindResource("SelectedButton");
+        }
+
+        private void setButtonColours()
+        {
+            System.Windows.Media.BrushConverter bc = new System.Windows.Media.BrushConverter();
+            App.Current.Resources["SelectedColour"] = (System.Windows.Media.Brush)bc.ConvertFrom("#FF68B9D2");
+            App.Current.Resources["NotSelectedColour"] = (System.Windows.Media.Brush)bc.ConvertFrom("#FF8ECADC");
+        }
        
     }
 }
