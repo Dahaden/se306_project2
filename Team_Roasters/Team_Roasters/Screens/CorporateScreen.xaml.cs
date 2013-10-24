af3cdf5b8db71b98f4a4c8ff8529b615b72500ae
Georgina Wood <georgina.wood@clear.net.nz>
2013-10-24 17:46:54 +1300

    All popup labels for small logos done.

--- Team_Roasters/Team_Roasters/Screens/CorporateScreen.xaml.cs
+++ Team_Roasters/Team_Roasters/Screens/CorporateScreen.xaml.cs
@@ -230,6 +230,7 @@ namespace Team_Roasters.Screens
             Prof_popup.Visibility = Visibility.Collapsed;
             BBnFreinds_text.Visibility = Visibility.Collapsed;
             _99_text.Visibility = Visibility.Collapsed;
+            Alabar_text.Visibility = Visibility.Collapsed;
         }
 
 		private void Mouse_down_BB(object sender, System.Windows.Input.MouseButtonEventArgs e){
@@ -275,7 +276,193 @@ namespace Team_Roasters.Screens
                 _99_text.Visibility = Visibility.Collapsed;
             }
         }
-		
+
+      
+        private void Mouse_Down_NZHerald(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            NZHerald_popup();
+        }
+
+        private void Touch_Down_NZHerald(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            NZHerald_popup();
+        }
+
+        private void NZHerald_popup()
+        {
+            if (The_Herald_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                The_Herald_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                The_Herald_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_Mill(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Mill_popup();
+        }
+
+        private void Touch_Down_Mill(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Mill_popup();
+        }
+
+        private void Mill_popup()
+        {
+            if (Mill_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Mill_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Mill_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_NZCouriers(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            NZCouriers_popup();
+        }
+
+        private void Touch_Down_NZCouriers(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            NZCouriers_popup();
+        }
+
+        private void NZCouriers_popup()
+        {
+            if (NZCouriers_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                NZCouriers_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                NZCouriers_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_Exposure(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Exposure_popup();
+        }
+
+        private void Touch_Down_Exposure(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Exposure_popup();
+        }
+
+        private void Exposure_popup()
+        {
+            if (Exposure_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Exposure_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Exposure_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_Hansen(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Hansen_popup();
+        }
+
+        private void Touch_Down_Hansen(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Hansen_popup();
+        }
+
+        private void Hansen_popup()
+        {
+            if (Hansen_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Hansen_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Hansen_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_ApparelMaster(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ApparelMaster_popup();
+        }
+
+        private void Touch_Down_ApparelMaster(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            ApparelMaster_popup();
+        }
+
+        private void ApparelMaster_popup()
+        {
+            if (Apparelmaster_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Apparelmaster_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Apparelmaster_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_Alabar(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Alabar_popup();
+        }
+
+        private void Touch_Down_Alabar(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Alabar_popup();
+        }
+
+        private void Alabar_popup()
+        {
+            if (Alabar_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Alabar_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Alabar_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Mouse_Down_Bluebridge(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Bluebridge_popup();
+        }
+
+        private void Touch_Down_Bluebridge(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Bluebridge_popup();
+        }
+
+        private void Bluebridge_popup()
+        {
+            if (Bluebridge_text.Visibility == Visibility.Collapsed)
+            {
+                Remove_popups();
+                Bluebridge_text.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Bluebridge_text.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
         private void Storyboard_Completed(object sender, EventArgs e)
         {
             parentWindow.Storyboard_Completed();
